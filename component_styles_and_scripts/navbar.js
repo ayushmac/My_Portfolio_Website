42ee249f35c2159639f8807ca7ab75436b40b5ed
Ayush Machha <ayushmac2002@gmail.com>
2023-08-22 13:56:54 +0530

    minor changes in navbar logic

--- component_styles_and_scripts/navbar.js
+++ component_styles_and_scripts/navbar.js
@@ -5,4 +5,19 @@ document.addEventListener("DOMContentLoaded", function () {
   hamIcon.addEventListener("click", function () {
     dropdownContainer.classList.toggle("open");
   });
+
+  document.addEventListener("click", function (event) {
+    const navigationBar = document.querySelector(".navigation-bar");
+
+    // Check if the clicked element is not inside the navigation bar
+    if (!navigationBar.contains(event.target)) {
+      const isOpen = hamIcon.classList.contains("open");
+
+      if (isOpen) {
+        // Close the dropdown
+        dropdownContainer.classList.remove("open");
+        hamIcon.classList.remove("open");
+      }
+    }
+  });
 });
